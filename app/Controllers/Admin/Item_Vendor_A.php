ba080a23e5b20631885b78212c13fe24614b070f
Muhammad Nazhim Maulana <nazhimmaulanamuhammad@gmail.com>
2021-11-12 17:53:06 +0700

    Menyiapkan vendor

--- app/Controllers/Admin/Item_Vendor_A.php
+++ app/Controllers/Admin/Item_Vendor_A.php
@@ -3,9 +3,12 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Entities\Item_Order_E;
+use App\Models\Vendor_M;
+use App\Entities\Vendor_E;
 
-use App\Models\Pesanan_M;
-use App\Entities\Pesanan_E;
+use App\Models\Pembelian_M;
+use App\Entities\Pembelian_E;
 
 use App\Models\Item_Vendor_M;
 use App\Entities\Item_Vendor_E;
@@ -26,7 +29,90 @@ class Item_Vendor_A extends BaseController
 
     public function buy()
     {
+        // Dapatkan Id dari segmen
+        $id_pembelian = $this->request->uri->getSegment(4);
 
-        return view('Admin_View/Item_Vendor_Admin/buy_item');
+        $model = new Item_Vendor_M();
+        $model_pembelian = new Pembelian_M();
+
+        $model_vendor = new Vendor_M();
+
+        $vendor = $model_vendor->findAll();
+        $list_vendor = [null => 'Pilih Satu'];
+
+        $list_item = [null => 'Pilih Satu', 'Kotoran Ayam' => 'Kotoran Ayam', 'Kotoran Sapi' => 'Kotoran Sapi', 'Kotoran Kambing' => 'Kotoran Kambing'];
+
+        // Buat looping
+        foreach ($vendor as $vendors) {
+            $list_vendor[$vendors->nama_vendor] = $vendors->nama_vendor;
+        }
+
+        $pembelian = $model->join('tbl_pembelian', 'tbl_pembelian.id_pembelian = tbl_item_vendor.id_pembelian')->join('tbl_vendor', 'tbl_vendor.id_vendor = tbl_item_vendor.id_vendor')->where('tbl_item_vendor.id_pembelian', $id_pembelian)->findAll();
+
+        // Mendapatkan Total Beli
+        $total_beli = $model->select('SUM(tbl_item_vendor.item_beli) AS jumlah')->where('tbl_item_vendor.id_pembelian', $id_pembelian)->get();
+
+        $data = [
+            'title' => 'Vendor Item',
+            'daftar_vendor' => $list_vendor,
+            'daftar_item' => $list_item,
+            'beli' => $pembelian,
+            'pembelian' => $model_pembelian->find($id_pembelian),
+            'total' => $total_beli->getResult()
+        ];
+
+        return view('Admin_View/Item_Vendor_Admin/buy_item', $data);
+    }
+
+
+    public function action()
+    {
+        if ($this->request->getVar('action')) {
+            $action = $this->request->getVar('action');
+
+            if ($action == 'get_vendor') {
+                $model = new Vendor_M();
+
+                $data = $model->where('nama_vendor', $this->request->getVar('nama_vendor'))->first();
+
+                echo json_encode($data);
+            }
+        }
+    }
+
+    public function tambah_pembelian()
+    {
+
+        $id_pembelian = $this->request->uri->getSegment(4);
+
+        if ($this->request->getPost()) {
+            // Jikalau ada data di post
+            $data_pembelian = $this->request->getPost();
+            $this->validation->run($data_pembelian, 'buy_item');
+            $errors = $this->validation->getErrors();
+
+            if (!$errors) {
+
+                // Simpan data
+                $model = new Item_Vendor_M();
+
+                $item = new Item_Order_E();
+
+                // Fill untuk assign value data kecuali gambar
+                $item->fill($data_pembelian);
+
+                //Input
+                $item->created_at = date("Y-m-d H:i:s");
+
+                $model->save($item);
+
+                $segments = ['Admin', 'Item_Vendor_A', 'buy', $id_pembelian];
+
+                // Akan redirect ke /Admin/Rak_A/view/$id_barang
+                return redirect()->to(site_url($segments));
+            }
+
+            $this->session->setFlashdata('errors', $errors);
+        }
     }
 }
--- app/Views/Admin_View/Item_Vendor_Admin/buy_item.php
+++ app/Views/Admin_View/Item_Vendor_Admin/buy_item.php
@@ -1,40 +1,218 @@
 <?= $this->extend('Template/Layouts/Admin') ?>
 <?= $this->section('content_admin') ?>
 <?php
-$keyword = [
-    'name' => 'keyword',
-    'value' => $keyword,
-    'placeholder' => 'Keyword Pencarian...',
-    'class' => 'form-control',
-    'type' => 'text'
+
+$id_pembelian = [
+    'name' => 'id_pembelian',
+    'id' => 'id_pembelian',
+    'readonly' => true,
+    'value' => $pembelian->id_pembelian,
+    'class' => 'form-control'
+];
+
+$lama_pesanan = [
+    'name' => 'lama_pesanan',
+    'id' => 'lama_pesanan',
+    'readonly' => true,
+    'value' => $pembelian->lama_pesanan,
+    'class' => 'form-control'
+];
+
+// Beli Item Vendor
+$nomor_pembelian = [
+    'name' => 'id_pembelian',
+    'id' => 'id_pembelian',
+    'type' => 'hidden',
+    'value' => $pembelian->id_pembelian,
+    'class' => 'form-control'
+];
+
+$nomor_vendor = [
+    'name' => 'id_vendor',
+    'id' => 'id_vendor',
+    'type' => 'hidden',
+    'value' => null,
+    'class' => 'form-control'
+];
+
+$nama_vendor = [
+    'name' => 'nama_vendor',
+    'id' => 'nama_vendor',
+    'options' => $daftar_vendor,
+    'class' => 'form-control'
+];
+
+$item_beli = [
+    'name' => 'item_beli',
+    'id' => 'item_beli',
+    'type' => 'number',
+    'value' => null,
+    'class' => 'form-control'
+];
+
+$nama_item = [
+    'name' => 'nama_item',
+    'id' => 'nama_item',
+    'options' => $daftar_item,
+    'class' => 'form-control'
 ];
 
 $submit = [
     'name' => 'submit',
-    'value' => 'Cari',
-    'type' => 'submit',
-    'class' => 'btn btn-outline-secondary'
+    'id' => 'submit',
+    'value' => 'Submit',
+    'class' => 'btn btn-success',
+    'type' => 'submit'
 ];
 
 $session = session();
+$errors = $session->getFlashdata('errors');
 ?>
 
 <h1 class="text-center">Pick Item</h1>
 
-<div class="row mt-5">
-    <div class="col-md-12">
-        <div class="card text-dark bg-light mb-3">
-            <div class="card-header"></div>
+<div class="container mt-4 vh-100">
+    <div class="row">
+        <div class="col-md-12 card">
+            <div class="card-header text-center">
+                <h5>Pilih Item dari Vendor</h5>
+            </div>
             <div class="card-body">
+                <?php if ($errors != null) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        <h4 class="alert-heading">Terjadi Kesalahan</h4>
+                        <hr>
+                        <p class="mb-0">
+                            <?php foreach ($errors as $err) {
+                                echo $err . '<br>';
+                            }
 
-                <p class="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            ?>
+                        </p>
+                    </div>
+                <?php endif ?>
+
+
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <?= form_label("Nomor Pembelian", "id_pembelian") ?>
+                        <?= form_input($id_pembelian) ?>
+                    </div>
+
+                    <div class="col-md-6 mb-4">
+                        <?= form_label("Lama Pesanan", "lama_pesanan") ?>
+                        <?= form_input($lama_pesanan) ?>
+                    </div>
+                </div>
+
+                <!-- Membuat Form dengan Form Helper -->
+                <?= form_open('Admin/Item_Vendor_A/tambah_pembelian/' . $pembelian->id_pembelian) ?>
+
+                <div class="row mt-5">
+                    <?= form_input($nomor_pembelian) ?>
+                    <?= form_input($nomor_vendor) ?>
+
+                    <div class="col-md-4 mb-4">
+                        <?= form_label("Nama Vendor", "nama_vendor") ?>
+                        <?= form_dropdown($nama_vendor) ?>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <?= form_label("Jumlah Pesanan (Kg)", "item_beli") ?>
+                        <?= form_input($item_beli) ?>
+                    </div>
+
+                    <div class="col-md-4 mb-4">
+                        <?= form_label("Nama Material", "nama_item") ?>
+                        <?= form_dropdown($nama_item) ?>
+                    </div>
+                </div>
+
+                <div class="mb-4 d-flex justify-content-center">
+
+                    <!-- Form submit terkait submit-->
+                    <?= form_submit($submit) ?>
+                </div>
+
+                <?= form_close() ?>
+
             </div>
-            <div class="card-footer">
-                <?= $pager->links('material', 'custom_pagination') ?>
-            </div>
+        </div>
+
+        <div class="col-lg-12 mt-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Nomor pembelian</th>
+                        <th scope="col">Nama Vendor</th>
+                        <th scope="col">Nama item</th>
+                        <th scope="col">Jumlah Beli</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php $i = 1; ?>
+                        <?php foreach ($beli as $index => $buys) : ?>
+                            <th scope="row"><?= $i++ ?></th>
+                            <td><?= $buys->id_pembelian ?></td>
+                            <td><?= $buys->nama_vendor ?></td>
+                            <td><?= $buys->nama_item ?></td>
+                            <td><?= $buys->item_beli ?></td>
+                            <td>
+                                <a href="#modalUpdate<?= $buys->id_item_vendor ?>" data-bs-toggle="modal" onclick="" class="btn btn-warning">Update</a>
+                                <a href="#modalDelete<?= $buys->id_item_vendor ?>" data-bs-toggle="modal" onclick="" class="btn btn-danger">Delete</a>
+                            </td>
+                    </tr>
+                <?php endforeach ?>
+
+                <tr>
+                    <td colspan="4">Total Beli (Kg)</td>
+                    <td colspan="2"><?= $total[0]->jumlah ?></td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
 
+<!-- Awal Modal Update -->
 
+
+<?= $this->endSection() ?>
+
+<!-- Bagian Script -->
+<?= $this->section('script') ?>
+
+<!-- Mendapatkan Harga Otomatis -->
+<script>
+    $(document).ready(function() {
+
+        $('#nama_vendor').change(function() {
+
+            var nama_vendor = $('#nama_vendor').val();
+
+            var action = 'get_vendor';
+
+            if (nama_vendor != '') {
+                $.ajax({
+                    url: "<?= base_url('Admin/Item_Vendor_A/action'); ?>",
+                    method: "POST",
+                    data: {
+                        nama_vendor: nama_vendor,
+                        action: action
+                    },
+                    dataType: "JSON",
+                    success: function(data) {
+                        $('#id_vendor').val(data.id_vendor);
+                    }
+                });
+
+            } else {
+                $('#id_vendor').val('');
+            }
+        });
+
+    });
+</script>
 <?= $this->endSection() ?>
